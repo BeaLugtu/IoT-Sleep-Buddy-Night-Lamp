89951f46f6f42e8f4e294e78f36ea06e15a830fb
Bea Alyssa Lugtu <149260052+BeaLugtu@users.noreply.github.com>
2024-12-18 05:37:37 +0800

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,42 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'light_control',
+// Create a new connection instance with MySQL parameters
+const connection = mysql.createConnection({
+  host: 'localhost', // Use your MySQL database host
+  user: 'root', // Replace with your MySQL username (default is 'root')
+  password: '', // Replace with your MySQL password (default is blank for phpMyAdmin)
+  database: 'light_control' // Replace with the MySQL database name
 });
 
-db.connect((err) => {
+// Connect to the MySQL database
+connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error('Connection error:', err.stack);
     return;
   }
-  console.log('MySQL connected...');
+  console.log('Connected to MySQL');
 });
 
-module.exports = db;
+module.exports = connection;
+
+
+
+// const mysql = require('mysql');
+
+// const db = mysql.createConnection({
+//   host: 'srv1785.hstgr.io',
+//   user: 'u280565526_smartglowteam',
+//   password: 'SmartGlow_2023',
+//   database: 'u280565526_SmartGlow',
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     return;
+//   }
+//   console.log('MySQL connected...');
+// });
+
+// module.exports = db;
+
